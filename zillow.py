76eb10e5dd43721fda15e5be52047a8025cbfd0a
EldHuang <eldridge.huang12@gmail.com>
2026-08-29 21:43:07 -0700

    Finish Zillow Portion of Project

--- zillow.py
+++ zillow.py
@@ -1,17 +1,30 @@
+import requests
 from bs4 import BeautifulSoup
 
 class Zillow:
     def __init__(self):
-        self.soup = BeautifulSoup("https://appbrewery.github.io/Zillow-Clone/", "html.parser")
+        url = requests.get(url="https://appbrewery.github.io/Zillow-Clone/")
+        self.soup = BeautifulSoup(url.text, "html.parser")
 
     def address(self):
-        pass
-        # RETURN LIST
+        addr_list = self.soup.find_all("address")
+        addr_list = [
+            item.text.split("|")[-1].strip() if "|" in item.text
+            else item.text.split(",", 1)[-1].strip()
+            for item in addr_list
+        ]
+        return addr_list
 
     def link(self):
-        pass
-        # RETURN LIST
+        links = self.soup.find_all("a", attrs={"data-test": "property-card-link"})
+        links = list(dict.fromkeys([link.get("href") for link in links]))
+        return links
 
     def price(self):
-        pass
-        # RETURN LIST
+        prices = [price.text.strip() for price in self.soup.find_all("span", attrs={"data-test": "property-card-price"})]
+        prices = [
+            price.split("+")[0] if "+" in price
+            else price.split("/")[0]
+            for price in prices
+        ]
+        return prices
