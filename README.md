bb7a8e2f514e0a382264c50e1317ed21ed854c67
EldHuang <eldridge.huang12@gmail.com>
2026-08-29 22:26:38 -0700

    Final Project

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # 52-webscraper-capstone_zillow
 Day 53/100 Udemy Python course. Webscraper python capstone project.
+Wrapping up webscraping section of course. ~10 Days. Using bs4 
+and selenium. 
+
+Purpose of Project: To practice real life examples of webscraping,
+and automation of jobs and applications.
+
+Project scrapes practice zillow site, and then takes (address, cost, link)
+and fills out a google form that is linked to a spreadsheet.
+Workers/Clients can then view that spreadsheet to get data.
--- forms.py
+++ forms.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class Forms:
     def __init__(self):
@@ -8,4 +11,41 @@ class Forms:
         self.driver.get("https://forms.gle/Jhv4CTfazUGUG3EE8")
 
     def fill_form(self, addr_list, price_list, links_list):
-        pass
+        wait = WebDriverWait(self.driver, 15)
+
+        for house in range(len(addr_list)):
+            address = wait.until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, "//*[@id='mG61Hd']/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input")
+                )
+            )
+
+            price = wait.until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, "//*[@id='mG61Hd']/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input")
+                )
+            )
+
+            link = wait.until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, "//*[@id='mG61Hd']/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input")
+                )
+            )
+
+            address.send_keys(addr_list[house])
+            price.send_keys(price_list[house])
+            link.send_keys(links_list[house])
+
+            submit_button = wait.until(
+                EC.element_to_be_clickable(
+                    (By.CSS_SELECTOR, "div[role='button']")
+                )
+            )
+            submit_button.click()
+
+            submit_another_response_btn = wait.until(
+                EC.element_to_be_clickable(
+                    (By.CSS_SELECTOR, "a[href*='viewform?usp=form_confirm']")
+                )
+            )
+            submit_another_response_btn.click()
